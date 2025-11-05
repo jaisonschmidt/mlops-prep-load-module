4ce148463961c03b27293d950c0afeb7e064d208
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-11-05 02:47:45 +0000

    Endpoints

--- app/main.py
+++ app/main.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({"message": "Hello World, Flask API is running!"})
 
+@app.route('/health-check', methods=['GET'])
+def get_status():
+    return jsonify({"status": "online", "framework": "Flask", "version": "1.0.0"})
+
+@app.route('/echo', methods=['POST'])
+def echo_data():
+    data = request.get_json()  # lê o corpo da requisição em JSON
+    return jsonify({
+        "received_data": data,
+        "message": "Dados recebidos com sucesso!"
+    })
+
+@app.route('/user/<name>', methods=['GET'])
+def get_user(name):
+    return jsonify({"message": f"Olá, {name}! Seja bem-vindo à API Flask."})
+
 if __name__ == '__main__':
     app.run(debug=True)
